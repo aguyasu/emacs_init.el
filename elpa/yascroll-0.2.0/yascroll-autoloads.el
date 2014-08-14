3a94f754879271d7da2f01bc2a0224f7fb27d428
aguyasu <mokada@mikilab.doshisha.ac.jp>
2014-08-14 16:24:23 +0900

    init.elあげます

---
+++ elpa/yascroll-0.2.0/yascroll-autoloads.el
@@ -0,0 +1,18 @@
+;;; yascroll-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("yascroll-pkg.el" "yascroll.el") (21480
+;;;;;;  62262 272226 0))
+
+;;;***
+
+(provide 'yascroll-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; yascroll-autoloads.el ends here
